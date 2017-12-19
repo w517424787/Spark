7a7e8e7363f00b8049b8d0bdfbdc1928aa9ad2fa
w517424787 <517424787@qq.com>
2017-12-19 16:43:43 +0800

    用spark.sparkContext代替sc

--- SparkExamples/BroadcastTest.scala
+++ SparkExamples/BroadcastTest.scala
@@ -12,7 +12,6 @@ object BroadcastTest {
     val blockSize = if (args.length > 2) args(2) else "4096"
     val spark = SparkSession.builder().appName("BroadcastTest").master("local[2]")
       .config("spark.broadcast.blockSize", blockSize).getOrCreate()
-    val sc = spark.sparkContext
 
     val slices = if (args.length > 0) args(0).toInt else 2
     val num = if (args.length > 1) args(1).toInt else 1000000
@@ -21,13 +20,12 @@ object BroadcastTest {
     for (i <- 0 until 3) {
       println("Iteration " + i)
       val startTime = System.nanoTime()
-      val broadArr = sc.broadcast(arr) //广播数组arr
-      val observedSizes = sc.parallelize(1 to 10, slices).map(_ => broadArr.value.length) //将RDD中的每个值替换为数组的长度
+      val broadArr = spark.sparkContext.broadcast(arr) //广播数组arr
+      val observedSizes = spark.sparkContext.parallelize(1 to 10, slices).map(_ => broadArr.value.length) //将RDD中的每个值替换为数组的长度
       observedSizes.collect().foreach(println)
       println("Iteration %d took %.0f milliseconds".format(i, (System.nanoTime - startTime) / 1E6))
     }
-	
-    sc.stop()
+
     spark.stop()
   }
 }
